8648300260d1ac386be44cda01f73413e511effd
Christian Marquez <ianmarquez939@yahoo.com.ph>
2014-03-18 20:52:59 +0800

    edit favicon

--- views/index.php
+++ views/index.php
@@ -7,13 +7,13 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-	<!--link rel="stylesheet/less" href="less/bootstrap.less" type="text/css" /-->
-	<!--link rel="stylesheet/less" href="less/responsive.less" type="text/css" /-->
-	<!--script src="js/less-1.3.3.min.js"></script-->
-	<!--append ‘#!watch’ to the browser URL, then refresh the page. -->
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-	<link href="../css/bootstrap.min.css" rel="stylesheet">
-	<link href="../css/style.css" rel="stylesheet">
+  <!--link rel="stylesheet/less" href="less/bootstrap.less" type="text/css" /-->
+  <!--link rel="stylesheet/less" href="less/responsive.less" type="text/css" /-->
+  <!--script src="js/less-1.3.3.min.js"></script-->
+  <!--append ‘#!watch’ to the browser URL, then refresh the page. -->
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+  <link href="../css/bootstrap.min.css" rel="stylesheet">
+  <link href="../css/style.css" rel="stylesheet">
 
   <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
   <!--[if lt IE 9]>
@@ -21,15 +21,15 @@
   <![endif]-->
 
   <!-- Fav and touch icons -->
-  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../img/apple-touch-icon-144-precomposed.png">
-  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../img/apple-touch-icon-114-precomposed.png">
-  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../img/apple-touch-icon-72-precomposed.png">
-  <link rel="apple-touch-icon-precomposed" href="../img/apple-touch-icon-57-precomposed.png">
-  <link rel="shortcut icon" href="../img/favicon.png">
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../img/logo-144.png">
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../img/logo-144.png">
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../img/logo-72.png">
+  <link rel="apple-touch-icon-precomposed" href="../img/logo-57.png">
+  <link rel="shortcut icon" href="../img/favicon-pup.png">
   
-	<script type="text/javascript" src="../js/jquery.min.js"></script>
-	<script type="text/javascript" src="../js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="../js/scripts.js"></script>
+  <script type="text/javascript" src="../js/jquery.min.js"></script>
+  <script type="text/javascript" src="../js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="../js/scripts.js"></script>
 </head>
 <body>
   <div class="container">
